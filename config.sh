7da733b491493a6514ad20440ad13388e010970e
kipper23132 <156115024+kipper23132@users.noreply.github.com>
2024-10-14 12:08:48 +0530

    Update config.sh
    Skk

--- config.sh
+++ config.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
-
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export FOX_BRANCH="android-12.1"
+export DT_LINK="https://github.com/s5e8535/twrp_device_samsung_m14x -b android-12.1"
+export DEVICE="m14x"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
